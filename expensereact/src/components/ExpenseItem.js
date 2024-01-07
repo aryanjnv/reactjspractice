a5d321955f7c8af972c0c01d37d0e562f293c7f6
aryanjnv <aryangpjnv188@gmail.com>
2024-01-07 21:15:52 +0530

    Styling and using JS in JSX

--- expensereact/src/components/ExpenseItem.js
+++ expensereact/src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
-    return<div>
-        <h2>Expense Item!</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+    const expenseDate=new Date(2023,2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=294.67;
+    const LocationOfExpenditure="Delhi"
+    return(
+    <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <p>{LocationOfExpenditure}</p>
+        <div className="expense-item__price">{expenseAmount}</div>
+        </div>
     </div>
+    );
 }
 
 export default ExpenseItem;
