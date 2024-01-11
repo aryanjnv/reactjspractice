053d2ef378b4f202ce6e8c3df17ad316ee0cfe0a
aryanjnv <aryangpjnv188@gmail.com>
2024-01-11 16:44:30 +0530

    Making Components Smaller and Readable assignment

---
+++ expensereact/src/components/ExpenseDetails.js
@@ -0,0 +1,12 @@
+import './ExpenseDetails.css';
+function ExpenseDetails(props){
+    return (
+<div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <p>{props.location}</p>
+        <div className="expense-item__price">{props.amount}</div>
+        </div>
+    );
+    
+}
+export default ExpenseDetails;
