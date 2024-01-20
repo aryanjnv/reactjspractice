a4f6b9891c8d37891b682f692e7d9327db59ca3a
aryanjnv <aryangpjnv188@gmail.com>
2024-01-20 20:55:18 +0530

    Title,Amount,Date Added

--- expensereact/src/components/Expenses/ExpenseItem.js
+++ expensereact/src/components/Expenses/ExpenseItem.js
@@ -6,32 +6,20 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  const [expense, setExpense] = useState(props.amount);
-
 
   const clickHandler = () => {
     setTitle('Updated!');
   };
 
-  const changeExpense = () => {
-    setExpense(100);
-  };
-
-  const deleteHandler = () => {
-    const cardElement = document.querySelector('.expense-item');
-    cardElement.remove();
-  };
-
+ 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>${expense}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={changeExpense}>Change Expenses</button>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={deleteHandler}>Delete</button>
     </Card>
   );
 };
