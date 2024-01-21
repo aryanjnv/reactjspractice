87ebac88ca78deed88dd6b721b23ad73fb9a5ba9
aryanjnv <aryangpjnv188@gmail.com>
2024-01-21 16:29:33 +0530

    PlaceHolder

--- expensereact/src/components/Expenses/Expenses.js
+++ expensereact/src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = (props) => {
+
+  const [filteredYear,setFilteredYear]=useState('2020');
+
+  const filterChangeHandler=selectedYear=>{
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+    <ExpenseFilter selected={filteredYear} on ChnageFilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
